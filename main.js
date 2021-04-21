713125259dedd73e50fea23b47faee04c86fe047
Ariadne030 <77916686+Ariadne030@users.noreply.github.com>
2021-04-21 12:36:09 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-
+const clockContainer = document.querySelector(".js-clock"),
+      clockTitle = clockContainer.querySelector("h1");
+var date= new Date(); //month 받아올 때엔 0부터 시작한다는 점 주의
+// new로 받아오면 생성된 시점으로 고정되는 것도 유의
+function getTime(){
+  
+}
+function init(){
+  
+}
+init();
